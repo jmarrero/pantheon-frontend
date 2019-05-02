45f48ee7e538809f8afbf28c85837e479e355467
Joseph Marrero <jmarrero@redhat.com>
2019-05-02 11:18:18 -0400

    more progress

--- src/app/module.tsx
+++ src/app/module.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput, BackgroundImage, BackgroundImageSrc, Button } from '@patternfly/react-core';
+import { Button, BackgroundImage, BackgroundImageSrc, TextInput } from '@patternfly/react-core';
 import '@app/app.css';
 import xs from '@assets/images/pfbg_576.jpg';
 import xs2x from '@assets/images/pfbg_576@2x.jpg';
@@ -20,57 +20,80 @@ const images = {
 
 export default class Module extends Component {
   public state = {
+    moduleName: '',
+    moduleDescription: '',
+    moduleFiles: null,
     redirect: false,
     failedPost: false
   };
+
   public render() {
+    const { moduleName, moduleDescription } = this.state;
     return (
       <React.Fragment>
         <BackgroundImage src={images} />
         <div className="app-container">
-          <div>
-            <TextInput id="module-name" value={"Module Name"} />
-            <TextInput id="module-description" value="Description" />
-            <input className="input-file" color="#dddddd" type="file" />
             <div>
-              {this.renderRedirect()}
-              <Button onClick={this.saveModule}>Save</Button>
+              <TextInput id="module-name" type="text" placeholder="Module Name" value={moduleName} onChange={this.handleTextInputChange1} />
+              <TextInput id="module-description" type="text" placeholder="Module Description" value={moduleDescription} onChange={this.handleTextInputChange2} />
+              <input id="input" className="input-file" color="#dddddd" type="file" onChange={(e) => this.handleFileChange(e.target.files)} />
+              <div>
+                {this.renderRedirect()}
+                <Button onClick={this.saveModule}>Save</Button>
+              </div>
             </div>
-          </div>
         </div>
       </React.Fragment>
     );
   }
 
+  handleTextInputChange1 = moduleName => {
+    this.setState({ moduleName });
+    console.log("Na " + moduleName)
+
+  };
+  handleTextInputChange2 = moduleDescription => {
+    this.setState({ moduleDescription });
+    console.log("Desc " + moduleDescription)
+  };
+
+  handleFileChange = selectorFiles => {
+    this.setState({ moduleFiles: selectorFiles })
+    console.log(selectorFiles);
+  }
+
   saveModule = (postBody) => {
-   // "jcr:title"=
-   // name="jcr:description"  
-   // name="asciidoc" type=file
-   // name=":checkinNewVersionableNodes" value="true"/> -->
-   // name="asciidoc@TypeHint" value="nt:file"/> -->
-   // name="sling:resourceType" value="pantheon/modules"/>
-   // name="jcr:primaryType" value="pant:module"/>
-   // name="asciidoc/jcr:content/jcr:mimeType" value="text/x-asciidoc"/>
+    console.log("My name is: "+ this.state.moduleName+ " and my desc is " + this.state.moduleDescription +" and my files are "+ this.state.moduleFiles)
+    // "jcr:title"=
+    // name="jcr:description"  
+    // name="asciidoc" type=file
+    // name=":checkinNewVersionableNodes" value="true"/> -->
+    // name="asciidoc@TypeHint" value="nt:file"/> -->
+    // name="sling:resourceType" value="pantheon/modules"/>
+    // name="jcr:primaryType" value="pant:module"/>
+    // name="asciidoc/jcr:content/jcr:mimeType" value="text/x-asciidoc"/>
 
- const   data = {"jcr:primaryType": 'pant:module',
-                "jcr:title": "Joseph",
-                "jcr:description": "Dance",
-                "sling:resourceType": 'pantheon/modules',
-                "pant:originalName": "",
-                "asciidoc@TypeHint": 'nt:file'}
+    const data = {
+      "jcr:primaryType": 'pant:module',
+      "jcr:title": this.state.moduleName,
+      "jcr:description": this.state.moduleDescription,
+      "sling:resourceType": 'pantheon/modules',
+      "pant:originalName": "",
+      "asciidoc@TypeHint": 'nt:file'
+    }
 
-        fetch('http://localhost:8080/content/modules/', {
-          method: 'post',
-          body: JSON.stringify(data)
-        }).then(response => {
-          if (response.status == 201) {
-            console.log(" Works "+response.status)
-           this.setState({redirect: true})
-          } else {
-            console.log(" Failed "+response.status)
-            this.setState({failedPost: true})
-          }
-        });
+    fetch('http://localhost:8080/content/modules/', {
+      method: 'post',
+      body: JSON.stringify(data)
+    }).then(response => {
+      if (response.status == 201) {
+        console.log(" Works " + response.status)
+        this.setState({ redirect: true })
+      } else {
+        console.log(" Failed " + response.status)
+        this.setState({ failedPost: true })
+      }
+    });
   }
 
   renderRedirect = () => {
